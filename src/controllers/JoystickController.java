ad0c438ba03325d5327c237b26696af306a34447
Combustible Lemons 5113 <frc5113@gmail.com>
2016-02-01 19:25:07 -0500

    Reverse Button
    Made a button on right joy button 4, reverses controls!

--- src/controllers/JoystickController.java
+++ src/controllers/JoystickController.java
@@ -3,18 +3,25 @@ package controllers;
 //This is for you Kyle :)
 
 import drive.MotorManager;
+
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import edu.wpi.first.wpilibj.buttons.Trigger.ButtonScheduler;
 
 public class JoystickController extends DriveController
 {
 	
 	Joystick leftStick;
 	Joystick rightStick;
+
+	private JoystickButton reverseControl;
 	
 	public void init() 
 	{	//The numbers are the USB port that the joysticks are plugged into
 		leftStick = new Joystick(0);//"Beat the devil out of it."
 		rightStick = new Joystick(1);//Ayy Lmao
+		
+		reverseControl = new JoystickButton(rightStick, 4);
 	}
 	
 	public void update(MotorManager dr) 
@@ -33,7 +40,16 @@ public class JoystickController extends DriveController
 		
 		if(rightYAxis < -0.99)
 			rightYAxis = -0.99;
+
+		boolean reverse = reverseControl.get();
+		
+		if (reverse == true)
+		{
+			leftYAxis = -leftYAxis;
+			rightYAxis = -rightYAxis;
+		}
 		
 		dr.tankDrive(leftYAxis, rightYAxis);
 	}
+	
 }
