69fc358dca2874022eaee1ff620a04530f59c8b4
FRC5113 <frc5113@gmail.com>
2016-02-12 17:17:29 -0500

    Moat/Rough Terrain
    Coded those two auton scenarios.

--- src/auton/RoughTerrain.java
+++ src/auton/RoughTerrain.java
@@ -4,9 +4,34 @@ import drive.SensorManager;
 
 public class RoughTerrain extends DefenseFrame
 {
-	@Override
+	private int caseSelector = 1;
+	private double speed = 0.5;
+	private double time;
+	
 	public void update(SensorManager sensors)
 	{
+		switch(caseSelector)
+		{
+		case 1:
+			controller.forward(speed);
+			time = System.currentTimeMillis();
+			
+			if(sensors.getGyroZAngle() > 5)
+				caseSelector = 2;
+			
+			break;
 		
+		case 2:
+			controller.forward(speed);
+			
+			if(sensors.getGyroZAngle() <= 5 && System.currentTimeMillis() - time > 5000)
+				caseSelector = 3;
+			
+			break;
+			
+		case 3:
+			System.out.println("Done!");
+				
+		}
 	}
 }
