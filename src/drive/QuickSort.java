5983f28e7d844ef638b984569938aa1a2ddca979
avr6130 <avr6130@gmail.com>
2016-02-20 00:05:50 -0500

    Circular buffer implementation and bug fixes
    Most of these changes were made by Felicia.

--- src/drive/QuickSort.java
+++ src/drive/QuickSort.java
@@ -1,76 +1,88 @@
 package drive;
 
-import java.util.*;
-import java.lang.*;
-import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
 
 public class QuickSort
 {
-	/**
-	 * Main method.
-	 * @param args
-	 */
-	private int arraySize;
-	private int currentIndex = 0;
-	//private ArrayList<Double> array;
-	
-	private Double[] array;
 
+	/** QuickSort constructor. This class is written in a generic manner and may be used in any situation where a list of 
+	 * values is needed from which a median may be calculated.
+	 * @param size - the maximum size of the list of values. 
+	 */
 	public QuickSort(int size){
 		this.arraySize = size;
-		//this.array = new ArrayList<Double>(size);
-		array = new Double [size];
+		this.fixedSizeArray = new Double [size];
 	}
-	
-	public static void main(String[] args){
 
-		QuickSort app = new QuickSort(2);
-		
+	/** addNewValue - Method to allow values to be added to the list of all values. This method should be called 
+	 * as often as necessary to provide an up to date list of values. It is from this list that a median is calculated.
+	 * @param value
+	 */
+	public void addNewValue(Double value)
+	{
 
-		 //Generate an integer array of length 7
-	    ArrayList<Double> input = app.generateRandomNumbers(8);
-		
-	    //Before sort
-	    System.out.println("input before\t" + input);
-	    
-	    //After sort
-	    ArrayList<Double> sortedData = app.sortData(input);
-	  
-//	    input = app.updateInput(input, 4);
-	    app.addNewMeasurement(99.987);
-	    sortedData = app.sortData(input);
-	    
-	    System.out.println("input after addition\t" + input);
-	    System.out.println("sortedData\t" + sortedData);
-	    
-	    System.out.println("Median\t" + app.findMedian(sortedData));
-	    
-	    //System.out.println("sortedData" + sortedData);
-	    
-	    
-	    app.addNewMeasurement(4.909);
-	    app.addNewMeasurement(3.67);
-	    app.addNewMeasurement(4.768);
-	    app.addNewMeasurement(2.665);
-	    app.addNewMeasurement(6.321);
-	    app.addNewMeasurement(6.883);
-	    app.addNewMeasurement(1.332);
-	    app.addNewMeasurement(4.768);
-	    
-	    System.out.println("Array output\t" + app.array);
-	    
-	    app.addNewMeasurement(9.765);
-	    app.addNewMeasurement(99.99);
-	    
-	    System.out.println("Array output after return to index 0\t" + app.array);
+		// Put the input into the class fixedSizeArray
+		fixedSizeArray[currentIndex] = value;
+
+		//increment index
+		currentIndex += 1;
+
+		// If the current index equals the length of the fixedSizeArray
+		if (currentIndex >= arraySize)
+
+			// Set the index back to zero so that the fixedSizeArray will wrap back to the beginning
+			// creating a circular buffer
+			currentIndex = 0;
 	}
+
+	/** getCurrentDistance 
+	 * @return double - This is a specifically named method for use by the Shooter, or any client requiring a normalized 
+	 * distance determined from the most recent measurements. 
+	 */
+	public Double getCurrentDistance() {
+
+		// Call the generic method of this class
+		return getMedian();
+	}
+
+	/** getMedian - Generic method to return the median value calculated from the array of all values
+	 * @return double - the median of the array of values from the most recent entries
+	 */
+	public Double getMedian() {
+
+		// Create a temp ordinary fixedSizeArray list to hold the sorted fixedSizeArray
+		ArrayList<Double> sortedData = new ArrayList<Double>();
+
+		// Copy the fixedSizeArray into an fixedSizeArray list
+		for (int i = 0; i < fixedSizeArray.length; i++){
+			sortedData.add(fixedSizeArray[i]);
+		}
+
+		// Sort the fixedSizeArray list
+		sortedData = quicksort(sortedData);
+
+		System.out.println("Sorted Data\n" + Arrays.toString(sortedData.toArray()));    
+
+		// Get the median of the sorted data and return it
+		return findMedian(sortedData);
+
+	}
+
+	/******************** Private Methods and Data Below *************************/
 	
-	private Double findMedian(ArrayList<Double> sortedData){
+	/**
+	 * @param sortedData
+	 * @return double representing the median of the list of values
+	 */
+	private Double findMedian(ArrayList<Double> sortedData) {
+
 		//assumes sortedData.size() != 0, >= 2
 		if (sortedData.size() % 2 == 0){
 			Integer medLoc1 = sortedData.size() / 2;
 			Integer medLoc2 = medLoc1 - 1;
-			
+
 			Double median = (sortedData.get(medLoc1) + sortedData.get(medLoc2)) / 2;
 			return median;
 		}
@@ -79,40 +91,25 @@ public class QuickSort
 			return sortedData.get(medLoc);
 		}
 	}
-	
-/*	private ArrayList<Double> updateInput(ArrayList<Double> input, double i){
-		input.remove(0);
-		input.add(i);
-		return input;
-	}*/
-	
-	private ArrayList<Double> sortData(List<Double> input){
-		ArrayList<Double> sortedData = new ArrayList<Double>();
-	    for (int i = 0; i < input.size(); i++){
-	    	sortedData.add(input.get(i));
-	    }
-	    sortedData = quicksort(sortedData);
-	    return sortedData;
-	}
-	
+
 	/**
-	 * This method sort the input ArrayList using quick sort algorithm.
-	 * @param input the ArrayList of integers.
-	 * @return sorted ArrayList of integers.
+	 * This method sorts the input Array using the quick sort algorithm.
+	 * @param ArrayList of doubles
+	 * @return sorted ArrayList of doubles.
 	 */
 	private ArrayList<Double> quicksort(ArrayList<Double> input){
-		
+
 		if(input.size() <= 1){
 			return input;
 		}
-		
-		//Integer middle = (Integer)Math.ceil(input.size() / 2);
+
+		//		Integer middle = (Integer)Math.ceil(input.size() / 2);
 		Integer middle = input.size() / 2;
 		Double pivot =  input.get(middle);
 
 		ArrayList<Double> less = new ArrayList<Double>();
 		ArrayList<Double> greater = new ArrayList<Double>();
-		
+
 		for (int i = 0; i < input.size(); i++) {
 			if(input.get(i) <= pivot){
 				if(i == middle){
@@ -124,66 +121,93 @@ public class QuickSort
 				greater.add(input.get(i));
 			}
 		}
-		
+
 		return concatenate(quicksort(less), pivot, quicksort(greater));
 	}
-	
+
 	/**
-	 * Join the less array, pivot integer, and greater array
-	 * to single array.
-	 * @param less integer ArrayList with values less than pivot.
+	 * Join the less fixedSizeArray, pivot integer, and greater fixedSizeArray
+	 * to single fixedSizeArray.
+	 * @param doubles integer ArrayList with values less than pivot.
 	 * @param pivot the pivot integer.
-	 * @param greater integer ArrayList with values greater than pivot.
+	 * @param doubles2 integer ArrayList with values greater than pivot.
 	 * @return the integer ArrayList after join.
 	 */
-	private ArrayList<Double> concatenate(ArrayList<Double> less, Double pivot, ArrayList<Double> greater){
-		
+	private ArrayList<Double> concatenate(ArrayList<Double> doubles, Double pivot, ArrayList<Double> doubles2){
+
 		ArrayList<Double> list = new ArrayList<Double>();
-		
-		for (int i = 0; i < less.size(); i++) {
-			list.add(less.get(i));
+
+		for (int i = 0; i < doubles.size(); i++) {
+			list.add(doubles.get(i));
 		}
-		
+
 		list.add(pivot);
-		
-		for (int i = 0; i < greater.size(); i++) {
-			list.add(greater.get(i));
+
+		for (int i = 0; i < doubles2.size(); i++) {
+			list.add(doubles2.get(i));
 		}
-		
+
 		return list;
 	}
-	
+
 	/**
-	 * This method generate a ArrayList with length n containing random integers . 
+	 * This method generates an ArrayList with length n containing random integers. 
+	 * It is used for testing purposes.
 	 * @param n the length of the ArrayList to generate.
 	 * @return ArrayList of random integers with length n. 
 	 */
 	private ArrayList<Double> generateRandomNumbers(int n){
-		
-	    ArrayList<Double> list = new ArrayList<Double>(n);
-	    Random random = new Random();
-		
-	    for (int i = 0; i < n; i++) {
-		    //list.add(random.nextInt(n * 10));
-	    	list.add(random.nextDouble() * 10);
-	    }
-	
-	    return list;
-	}
-	
-	public void addNewMeasurement(Double range)
-	{
-		if(currentIndex >= array.length)
-			currentIndex = 0;
 
-//		if(currentIndex < arraySize)
-			array[currentIndex] = range;
-				
-		//increment index
-		currentIndex += 1;
-		//if index is > array size set index = 0;
-	}
-	
-	
+		ArrayList<Double> list = new ArrayList<Double>(n);
+		Random random = new Random();
 
-}
+		for (int i = 0; i < n; i++) {
+			//list.add(random.nextInt(n * 10));
+			list.add(random.nextDouble() * 10);
+		}
+
+		return list;
+	}
+
+	// Private class variables
+	private int arraySize;
+
+	// A basic array is used instead of an ArrayList to make circular buffer and indexing easier  
+	private Double[] fixedSizeArray;
+
+	// Variable used for the circular buffer logic
+	private int currentIndex = 0;
+
+	public static void main(String[] args) {
+
+		QuickSort app = new QuickSort(8);
+
+		app.addNewValue(4.909);
+		app.addNewValue(3.67);
+		app.addNewValue(4.768);
+		app.addNewValue(2.665);
+		app.addNewValue(6.321);
+		app.addNewValue(6.883);
+		app.addNewValue(1.332);
+		app.addNewValue(3.768);
+		app.addNewValue(8.665);
+		app.addNewValue(634.321);
+		app.addNewValue(6.883);
+		app.addNewValue(11.332);
+		app.addNewValue(4.768);
+
+		System.out.println("Original Array output");
+		System.out.println(Arrays.toString(app.fixedSizeArray));
+
+		app.addNewValue(9.765);
+		app.addNewValue(99.99);
+
+//		Debug output statements
+//		System.out.println("Array output after wrap to index 0");
+//		System.out.println(Arrays.toString(app.fixedSizeArray));
+
+		System.out.println(app.getCurrentDistance());
+		
+	} // end main 
+
+} // end class QuickSort
